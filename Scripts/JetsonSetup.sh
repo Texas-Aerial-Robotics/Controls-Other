d821175be7621d08d11b5349770d6aa4852ee918
Umer Salman <umer936@hotmail.com>
2018-02-26 23:45:41 -0600

    Add more packages to remove in JetsonSetup.sh

--- Scripts/JetsonSetup.sh
+++ Scripts/JetsonSetup.sh
@@ -8,17 +8,18 @@ fi
 set -e
 set -x
 
-export TELEM_SERIAL_PORT=/dev/ttyS0
+# export TELEM_SERIAL_PORT=/dev/ttyS0
 chmod 666 /dev/ttyS0
 
 # avahi-daemon for "ssh apsync" over WiFi
 
 # Mess with packages 
-apt-get purge -y whoopsie modemmanager unattended-upgrades deja-dup libreoffice-* gnome-mines gnome-sudoku gnome-mahjongg aisleriot  
 apt-get update
-apt-get install -y git tcpdump lsof mlocate v4l-utils usbutils tree
-apt-get dist-upgrade -y
+apt-get purge -y whoopsie cups modemmanager unattended-upgrades deja-dup libreoffice-* gnome-mines gnome-sudoku gnome-mahjongg gnome-screensaver rhythmbox example-content telepathy aisleriot account-plugin-* brltty duplicity empathy thunderbird
 apt-get autoremove -y
+apt-get install -y git tcpdump lsof mlocate v4l-utils usbutils tree
+apt-get update
+apt-get dist-upgrade -y
 apt-get clean
 
 for ID in $(cat /etc/passwd | grep /home | cut -d ':' -f1); do (adduser $ID dialout);done
