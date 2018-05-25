294ce1e07feb37a2007017f98c6c1d7daf497e73
ericjohnson97 <eric.emj.johnson@gmail.com>
2018-05-25 15:04:24 -0500

    updated script to log data

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -8,6 +8,8 @@ roslaunch mavros apm.launch fcu_url:="/dev/ttyS0:921600" &
 
 sleep 3
 
+sudo chmod 777 /dev/ttyS0
+
 rosrun mavros mavsys rate --all 10 &&
 
 rosrun flight_pkg setHome
@@ -20,15 +22,15 @@ roslaunch darknet_ros darknet_ros.launch &
 
 sleep 5
 
-rosrun transformations_ros simpleTransform &
+rosrun transformations_ros simpleTransform >> simpleTransform.log &
 
 sleep 1
 
-roslaunch flight_pkg follow.launch &
+roslaunch flight_pkg follow.launch >> follow.log &
 
 sleep 1
 
-rosrun stratnode lin_reg &
+rosrun stratnode lin_reg >> lin_reg.log &
 
 
 
