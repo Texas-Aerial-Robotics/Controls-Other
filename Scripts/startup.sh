0fad3b997bcbaa52c24c1c570c53001037048959
UT <texasaerialrobotics@gmail.com>
2018-07-15 00:08:58 +0000

    Startup script now writes processIds to a file and killStartup kills all those processes that are written

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -2,35 +2,57 @@ export DISPLAY=:0
 
 sudo chmod 777 /dev/ttyS0
 
+echo "" > lastRunProcIds
+
 roscore &
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
 sleep 3
 
 roslaunch mavros apm.launch fcu_url:="/dev/ttyS0:921600" &
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
 sleep 3
 
 rosrun mavros mavsys rate --all 10 &&
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
 rosrun flight_pkg setHome
 
 roslaunch video_stream_opencv droneMultipleCams.launch &
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
 roslaunch camera_signalman drone.launch &
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
 sleep 2
 
 roslaunch darknet_ros darknet_ros.launch &
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
 sleep 5
 
 roslaunch transformations_ros  transformations.launch &
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
 sleep 1
 
-roslaunch flight_pkg followScan.launch &
+echo "" > controls.log
+roslaunch flight_pkg followScan.launch >> controls.log &
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
 sleep 1
 
-rosrun stratnode stratnode &
+echo "" > stratnode.log
+rosrun stratnode stratnode >> stratnode.log &
+echo -n $! >> lastRunProcIds
+echo -n " " >> lastRunProcIds
 
