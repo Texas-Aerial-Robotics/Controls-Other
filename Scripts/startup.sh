7556776632354e438dfbab30c3e1a903fb3e0374
Umer Salman <umer936@hotmail.com>
2018-07-15 20:12:00 -0500

    Startup has some fancy logging stuff it's pretty cool

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -4,6 +4,14 @@ sudo chmod 777 /dev/ttyS0
 
 echo "" > lastRunProcIds
 
+current_date_time="`date +%Y-%m-%d_%H-%M-%S`"
+echo $current_date_time
+
+mkdir -p ~/TAR_logs/$current_date_time
+
+rm latest
+ln -s ~/TAR_logs/$current_date_time ~/TAR_logs/latest
+
 roscore &
 echo -n $! >> lastRunProcIds
 echo -n " " >> lastRunProcIds
@@ -22,7 +30,8 @@ echo -n " " >> lastRunProcIds
 
 rosrun flight_pkg setHome
 
-roslaunch video_stream_opencv droneMultipleCams.launch &
+echo "" > ~/TAR_logs/$current_date_time/video_stream_opencv.log
+roslaunch video_stream_opencv droneMultipleCams.launch | sed 's/[\x01-\x1F\x7F]//g' | sed 's/\[0m//g' >> ~/TAR_logs/$current_date_time/video_stream_opencv.log &
 echo -n $! >> lastRunProcIds
 echo -n " " >> lastRunProcIds
 
@@ -32,27 +41,29 @@ echo -n " " >> lastRunProcIds
 
 sleep 2
 
-roslaunch darknet_ros darknet_ros.launch &
+echo "" > ~/TAR_logs/$current_date_time/darknet.log
+roslaunch darknet_ros darknet_ros.launch | sed 's/[\x01-\x1F\x7F]//g' | sed 's/\[0m//g' >> ~/TAR_logs/$current_date_time/darknet.log &
 echo -n $! >> lastRunProcIds
 echo -n " " >> lastRunProcIds
 
 sleep 5
 
-roslaunch transformations_ros  transformations.launch >> transformations.log &
+echo "" > ~/TAR_logs/$current_date_time/transformations.log
+roslaunch transformations_ros  transformations.launch >> ~/TAR_logs/$current_date_time/transformations.log &
 echo -n $! >> lastRunProcIds
 echo -n " " >> lastRunProcIds
 
 sleep 1
 
-echo "" > controls.log
-roslaunch flight_pkg followScan.launch >> controls.log &
+echo "" > ~/TAR_logs/$current_date_time/controls.log
+roslaunch flight_pkg followScan.launch | sed 's/[\x01-\x1F\x7F]//g' | sed 's/\[0m//g' >> ~/TAR_logs/$current_date_time/controls.log &
 echo -n $! >> lastRunProcIds
 echo -n " " >> lastRunProcIds
 
 sleep 1
 
-echo "" > stratnode.log
-rosrun stratnode stratnode >> stratnode.log &
+echo "" > ~/TAR_logs/$current_date_time/stratnode.log
+rosrun stratnode stratnode >> ~/TAR_logs/$current_date_time/stratnode.log &
 echo -n $! >> lastRunProcIds
 echo -n " " >> lastRunProcIds
 
